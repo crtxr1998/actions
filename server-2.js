b0ec0dab2f9011ff77070b969de59f006979d81e
itxue <33005087+crtxr1998@users.noreply.github.com>
2020-10-28 17:42:47 +0800

    Add files via upload

---
+++ server-2.js
@@ -0,0 +1,26 @@
+/* 安装  初始化 引入 */
+var https = require("https");
+var express = require("express");
+
+/* 创建服务器 */
+var app = express();
+
+/* 接受请求并反馈数据渲染到界面*/
+app.get("/", function (req, res) {
+    res.send("I am Server-2-----");
+})
+
+app.listen(5000, function () {
+    https.get("https://api.uomg.com/api/visitor.info?skey=" + Date.now, function (res) {
+        res.on("data", function (chunk) {
+            console.log("on data......");
+            var result = JSON.parse(JSON.parse(JSON.stringify(chunk + "")))
+            console.log(result);
+        });
+        res.on("end", function (chunk) {
+            console.log("request  end ......");
+        });
+        console.log("send request");
+    })
+    console.log("Server is running....");
+})
