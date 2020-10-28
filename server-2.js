8da322b0f066e5e3039b4cc2f076cd579c1a9531
itxue <33005087+crtxr1998@users.noreply.github.com>
2020-10-28 21:18:49 +0800

    Update server-2.js

--- server-2.js
+++ server-2.js
@@ -20,7 +20,6 @@ var app = express();
 //     });
 //     console.log("send request");
 // })
-// app.listen(5000, function () {
-
-//     console.log("Server is running....");
-// })
+app.listen(5000, function () {
+    console.log("Server is running....");
+})
